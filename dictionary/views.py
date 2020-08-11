9042bbd9db6c635ed046b14113953c2a91f8880c
Yubin Lee <lybin10@naver.com>
2020-08-12 03:04:52 +0900

    SWU DICTIONARY

--- dictionary/views.py
+++ dictionary/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from . models import Post
 # Create your views here.
+
+
+def index(request):
+    posts = Post.objects.all()
+
+    return render(
+        request,
+        'dictionary/index.html',
+        {
+            'posts': posts,
+        }
+    )
